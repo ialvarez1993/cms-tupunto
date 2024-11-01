f16e8209ec18c169230b35249576f65ecce51d49
Ivan Alvarez <i.alvarez@tiendasdaka.com>
2024-11-01 15:19:15 -0400

    feat(components): initial config cta

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,22 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedCta extends Struct.ComponentSchema {
+  collectionName: 'components_shared_ctas';
+  info: {
+    description: '';
+    displayName: 'CTA';
+    icon: 'apps';
+  };
+  attributes: {
+    media: Schema.Attribute.Media<'images' | 'files'>;
+    url: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.cta': SharedCta;
+    }
+  }
+}
