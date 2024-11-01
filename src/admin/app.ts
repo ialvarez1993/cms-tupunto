75781f104eec38d11e6744229b70a8a82ed826b7
Ivan Alvarez <i.alvarez@tiendasdaka.com>
2024-11-01 09:19:49 -0400

    feat(theme): locale and colors

--- src/admin/app.ts
+++ src/admin/app.ts
@@ -1,14 +1,30 @@
-import type { StrapiApp } from '@strapi/strapi/admin';
+import type { StrapiApp } from "@strapi/strapi/admin";
 
 export default {
   config: {
+    theme: {
+      light: {
+        colors: {
+          primary600: "orange",
+          buttonPrimary500: "orange",
+          buttonPrimary600: "orange",
+        },
+      },
+      dark: {
+        colors: {
+          primary600: "orange",
+          buttonPrimary500: "orange",
+          buttonPrimary600: "orange",
+        },
+      },
+    },
     locales: [
       // 'ar',
       // 'fr',
       // 'cs',
       // 'de',
       // 'dk',
-      // 'es',
+      "es",
       // 'he',
       // 'id',
       // 'it',
